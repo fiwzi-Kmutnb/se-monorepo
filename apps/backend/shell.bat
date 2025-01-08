5c90f02ac045dd35a336a6552b84ed0e77d3625c
fiwzi <garfild5512@gmail.com>
2025-01-09 01:35:03 +0700

    feat: fix .bat

--- apps/backend/shell.bat
+++ apps/backend/shell.bat
@@ -9,8 +9,8 @@ pnpm nest g mo api/v1/%name%
 pnpm nest g s api/v1/%name%/guest/%name%.guest --flat
 pnpm nest g co api/v1/%name%/guest/%name%.guest --flat
 
-pnpm nest g s api/v1/\%name%\authroized/%name%.authroized --flat
-pnpm nest g co api/v1\%name%\authroized/%name%.authroized --flat
+pnpm nest g s api/v1/%name%/authroized/%name%.authroized --flat
+pnpm nest g co api/v1/%name%/authroized/%name%.authroized --flat
 
 pnpm nest g s api/v1/%name%/restricted/%name%.authroized --flat
 pnpm nest g co api/v1/%name%/restricted/%name%.authroized --flat
