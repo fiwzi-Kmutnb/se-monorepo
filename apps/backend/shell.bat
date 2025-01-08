b58b3d62611be7e82ad1d3e4b178496b4f7b0589
fiwzi <garfild5512@gmail.com>
2025-01-09 01:46:08 +0700

    feat: fix .bat v3

--- apps/backend/shell.bat
+++ apps/backend/shell.bat
@@ -1,18 +1,18 @@
 @echo off
 set /p name=Enter the name: 
 
-pnpm nest g mo api/v1/%name%
+pnpm nest g mo api/v1/%name% &&
 
-pnpm nest g s api/v1/%name%/internal/%name%.internal --flat
-pnpm nest g co api/v1/%name%/internal/%name%.internal --flat
+@REM pnpm nest g s api/v1/%name%/internal/%name%.internal --flat &&
+@REM pnpm nest g co api/v1/%name%/internal/%name%.internal --flat &&
 
-pnpm nest g s api/v1/%name%/guest/%name%.guest --flat
-pnpm nest g co api/v1/%name%/guest/%name%.guest --flat
+pnpm nest g s api/v1/%name%/guest/%name%.guest --flat &&
+pnpm nest g co api/v1/%name%/guest/%name%.guest --flat &&
 
-pnpm nest g s api/v1/%name%/authorized/%name%.authorized --flat
-pnpm nest g co api/v1/%name%/authorized/%name%.authorized --flat
+pnpm nest g s api/v1/%name%/authorized/%name%.authorized --flat &&
+pnpm nest g co api/v1/%name%/authorized/%name%.authorized --flat &&
 
-pnpm nest g s api/v1/%name%/restricted/%name%.restricted --flat
+pnpm nest g s api/v1/%name%/restricted/%name%.restricted --flat &&
 pnpm nest g co api/v1/%name%/restricted/%name%.restricted --flat
 
 echo Generation complete for: %name%
