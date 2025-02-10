b16ac1302a3cfb5fd64b86ccd077adf9da57b45a
Teerapan Lerdtanaseangtham <garfild5512@gmail.com>
2025-02-10 22:03:10 +0700

    feat(frontend): create slidebar

---
+++ apps/frontend/src/pages/test/index.jsx
@@ -0,0 +1,21 @@
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+
+export default function Test() {
+  return (
+    <>
+      {/* <div className="bg-white shadow-md w-[320px] h-screen">
+        <div className="flex flex-col h-screen">
+          <div className="mt-2">
+            <img src="./logo.svg" className="w-52 mx-auto" alt="" />
+          </div>
+          <div className="px-3 w-full text-left mt-2">
+            <div>
+            <button className="bg-[#2B2D42] w-full text-left rounded-xl text-white p-3 flex gap-2 "><MdOutlineSpaceDashboard size={20} /> Dashborads</button>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      a
+    </>
+  );
+}
