ad12cbc1cfc4d087a8951e0692ad081047770eed
Kanun <nonthachai.3003@gmail.com>
2025-02-13 22:34:21 +0700

    feat (fontend): HistoryOrder

--- apps/frontend/src/pages/index.js
+++ apps/frontend/src/pages/index.js
@@ -1,20 +1,15 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Prompt, } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const fomtPrompt = Prompt({
+  weight: ["400"],
   subsets: ["latin"],
 });
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${fomtPrompt.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
